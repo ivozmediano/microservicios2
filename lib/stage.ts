be0d257195b566693223fb955533b3c1b5e8263b
ivozmediano <p-ivozmediano@neoris.com>
2023-05-29 10:00:03 +0200

    Update stage.ts

--- lib/stage.ts
+++ lib/stage.ts
@@ -3,7 +3,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { MyLamdaStack } from './lambda-stack';
 
-//
+//Definición de la clase stage/etapa
 export class MyPipelineAppStage extends cdk.Stage {
     constructor(scope: Construct, stageName: string, props?: cdk.StackProps) {
         super(scope, stageName, props);
